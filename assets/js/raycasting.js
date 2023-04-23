951d893c2bb8cd4b5508d4d8ab3d6a0fcf9982ca
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-04-23 12:06:13 +0200

    other alg finished

--- assets/js/raycasting.js
+++ assets/js/raycasting.js
@@ -10,71 +10,41 @@ class Raycasting {
     rayCast() {
         this.rayCastingResult = [];
         const origin = this.game.player.pos;
-        const mapOrigin = this.game.player.mapPos;
+        // const mapOrigin = this.game.player.mapPos;
 
         let rayAngle = this.game.player.angle - (this.game.player.fov / 2) + 0.00001;
         for (let ray = 0; ray < this.rayCount; ray++) {
             const sinAngle = Math.sin(rayAngle);
             const cosAngle = Math.cos(rayAngle);
 
-            // check horizontal collisions
-            const stepHor = {};
-            const intersectHor = {};
-            if (sinAngle > 0) {
-                stepHor.dy = 1;
-                intersectHor.y = mapOrigin.row + 1;
-            } else {
-                stepHor.dy = -1;
-                intersectHor.y = mapOrigin.row - 1e-6;
+            const distOne = {
+                x: 1 / Math.abs(cosAngle),
+                y: 1 / Math.abs(sinAngle)
+            }
+            const distNext = {
+                x: cosAngle < 0 ? (origin.x % 1) * distOne.x + 1e-6 : (1 - (origin.x % 1)) * distOne.x + 1e-6,
+                y: sinAngle < 0 ? (origin.y % 1) * distOne.y + 1e-6 : (1 - (origin.y % 1)) * distOne.y + 1e-6
             }
 
-            let depthHor = (intersectHor.y - origin.y) / sinAngle;
-            intersectHor.x = origin.x + depthHor * cosAngle;
+            let distance;
+            while (true) {
+                if (distNext.x < distNext.y) {
+                    distance = distNext.x;
+                    distNext.x += distOne.x;
+                } else if (distNext.y < distNext.x) {
+                    distance = distNext.y;
+                    distNext.y += distOne.y;
+                } else {
+                    distance = distNext.x;
+                    distNext.x += distOne.x;
+                    distNext.y += distOne.y;
+                }
 
-            const deltaDepthHor = stepHor.dy / sinAngle;
-            stepHor.dx = deltaDepthHor * cosAngle;
-
-            for (let i = 0; i < this.maxRayDepth; i++) {
-                const tileCoords = {col: Math.floor(intersectHor.x), row: Math.floor(intersectHor.y)};
-                if (this.game.map.isWall(tileCoords.col, tileCoords.row)) {
+                const curPos = {x: origin.x + distance * cosAngle, y: origin.y + distance * sinAngle};
+                if (this.game.map.isWall(Math.floor(curPos.x), Math.floor(curPos.y))) {
+                    this.rayCastingResult.push([origin.x, origin.y, curPos.x, curPos.y]);
                     break
                 }
-                intersectHor.y += stepHor.dy;
-                intersectHor.x += stepHor.dx;
-                depthHor += deltaDepthHor;
-            }
-
-            // check vertical collisions
-            const stepVert = {};
-            const intersectVert = {};
-            if (cosAngle > 0) {
-                stepVert.dx = 1;
-                intersectVert.x = mapOrigin.col + 1;
-            } else {
-                stepVert.dx = -1;
-                intersectVert.x = mapOrigin.col - 1e-6;
-            }
-
-            let depthVert = (intersectVert.x - origin.x) / cosAngle;
-            intersectVert.y = origin.y + depthVert * sinAngle;
-
-            const deltaDepthVert = stepVert.dx / cosAngle;
-            stepVert.dy = deltaDepthVert * sinAngle;
-
-            for (let i = 0; i < this.maxRayDepth; i++) {
-                const tileCoords = {col: Math.floor(intersectVert.x), row: Math.floor(intersectVert.y)};
-                if (this.game.map.isWall(tileCoords.col, tileCoords.row)) {
-                    break
-                }
-                intersectVert.y += stepVert.dy;
-                intersectVert.x += stepVert.dx;
-                depthVert += deltaDepthVert;
-            }
-
-            if (depthVert < depthHor) {
-                this.rayCastingResult.push([origin.x, origin.y, intersectVert.x, intersectVert.y]);
-            } else {
-                this.rayCastingResult.push([origin.x, origin.y, intersectHor.x, intersectHor.y]);
             }
 
             rayAngle += this.game.player.fov / this.rayCount;
